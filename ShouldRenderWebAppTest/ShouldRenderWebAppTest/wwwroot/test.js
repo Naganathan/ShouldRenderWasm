85b93e67ba6f57575b575a3b7dd0f7c4756b38df
naganthanganeshbabu <naganathanganeshbabu@syncfusion.com>
2020-10-08 19:53:19 +0530

    Should render simple sample with 1000 elements

---
+++ ShouldRenderWebAppTest/ShouldRenderWebAppTest/wwwroot/test.js
@@ -0,0 +1,8 @@
+﻿window.getDiagramSize = function () {
+    var element = document.getElementById("canvas");
+    var bounds = element.getBoundingClientRect();
+    var size = {};
+    size["X"] = bounds.left;
+    size["Y"] = bounds.top;
+    return size;
+}
